fbdf3fcc6351027e3e36613384cf09685132c3fc
669823 <669823@stud.hvl.no>
2023-09-11 16:04:27 +0200

    commit1

--- f07-eksempler-skeleton/src/busywait/Main.java
+++ f07-eksempler-skeleton/src/busywait/Main.java
@@ -1,5 +1,6 @@
 package busywait;
 
+
 public class Main {
 	
 	private static String melding;
@@ -7,10 +8,30 @@ public class Main {
 	public static void main(String[] args) {
 		
 		//En tråd som skriver ut melding på skjermen
-//		Thread printlnTraad = 
+		Thread printlnTraad = new Thread(){
+			@Override
+			public void run(){
+				while(melding == null){
+					try{Thread.sleep(10);}
+					 catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				System.out.println(melding);
+			}
+		};
 		
 		//En tråd som gir melding en verdi
-//		Thread giVerdiTraad = 
 		
-	}
-}
+		Thread giVerdiTraad = new Thread(() -> melding = "Hello");
+		printlnTraad.start();
+		giVerdiTraad.start();
+
+
+		
+			
+		
+	
+		
+	}};
+
--- f07-eksempler-skeleton/src/waitnotify/Main.java
+++ f07-eksempler-skeleton/src/waitnotify/Main.java
@@ -1,7 +1,45 @@
 package waitnotify;
 
+
 public class Main {
 	
-	//Kopiere fra forrige og ta det derfra
-
+	private static String melding;
+	
+	public static void main(String[] args) {
+		Object lock = new Object();
+		//En tråd som skriver ut melding på skjermen
+		Thread printlnTraad = new Thread(){
+			@Override
+			public void run(){
+				while(melding == null){
+					try {
+						synchronized(lock){
+							lock.wait();
+						}
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				System.out.println(melding);
+			}
+		};
+		
+		//En tråd som gir melding en verdi
+		
+		Thread giVerdiTraad = new Thread(() -> {melding = "Hello"; 
+		synchronized(lock){
+			lock.notifyAll();
+		}
+	});
+		printlnTraad.start();
+		giVerdiTraad.start();
+		
 }
+}
+		
+			
+		
+	
+		
+	
+
